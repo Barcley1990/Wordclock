185096aa8a7fa077d4e08f3a2e93fe5d091ce1b3
Tobias Nuss <tnuss90@gmail.com>
2023-03-25 10:48:49 +0100

    Crude system counter algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,54 @@
 #include "wordclock.h"
 
 //=================================================
+#define OS_1MS_EVENT    1
+#define OS_10MS_EVENT   10
+#define OS_100MS_EVENT  100
+#define OS_1000MS_EVENT 1000
+
+//=================================================
 #ifdef DEBUG_MODE
   uint8_t DEBUG_HOUR = 0;
   uint8_t DEBUG_MINUTE = 0;
 #endif
-static uint32_t WordClock_SysTimerCnt;
-bool WordClock_1000ms_Ev = false;
+static uint16_t Os_SysTimerCnt;
 
 //=================================================
 
 
 //=================================================
-void setup() {
-  WordClock_SysTimerCnt = 0;
+void setup() 
+{
+  Os_SysTimerCnt = 0;
 }
 
 //=================================================
-void loop() {
-  // Every second
-  if(millis() > WordClock_SysTimerCnt)
+void loop() 
+{
+  uint32_t sys_time = millis();
+  static uint32_t old_time = 0u;
+
+  if((sys_time - old_time) >= OS_10MS_EVENT)
   {
-    WordClock_Runnable_1s();
-    WordClock_SysTimerCnt += 1000;
+    Os_SysTimerCnt++;
+    old_time = sys_time;
   }
+
+  if((Os_SysTimerCnt % OS_100MS_EVENT) >= OS_100MS_EVENT)
+  {
+    // Exec
+
+  }
+
+  if((Os_SysTimerCnt % OS_1000MS_EVENT) >= OS_1000MS_EVENT)
+  {
+    // Exec
+    WordClock_Runnable_1s();
+  }
+
+  // Reset counter
+  if(Os_SysTimerCnt >= 1000u)
+    Os_SysTimerCnt = 0u;
 }
 
 
